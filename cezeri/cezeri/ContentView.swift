5a0196aaca38dd6e694dfaca8eab0309bfa92c68
Elif Sude Meydan <elifsudemeydan@Elif-MacBook-Pro.local>
2020-10-26 23:08:27 +0300

    boş proje oluşturuldu

---
+++ cezeri/cezeri/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  cezeri
+//
+//  Created by Elif Sude Meydan on 26.10.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
